43cccb8a792d5f2496632f897bd59d174934e7d3
Paul Griffiths <paul@paulgriffiths.net>
2020-03-13 13:57:23 -0400

    Update to use pgtpm package.

--- getcaps.go
+++ getcaps.go
@@ -8,6 +8,12 @@ import (
 
 	"github.com/google/go-tpm/tpm2"
 	"github.com/google/go-tpm/tpmutil"
+
+	"github.com/paulgriffiths/pgtpm"
+)
+
+const (
+	capRequestSize = 16
 )
 
 // outputCaps outputs selected TPM capabilities.
@@ -17,14 +23,14 @@ func outputCaps() {
 
 	for _, c := range []struct {
 		process bool
-		id      uint32
+		id      pgtpm.Capability
 		cmdFunc func(io.ReadWriteCloser)
 	}{
-		{*fCapsAlgs, TPM2_CAP_ALGS, outputCapsAlgorithms},
-		{*fCapsHandles, TPM2_CAP_HANDLES, outputCapsHandles},
+		{*fCapsAlgs, pgtpm.TPM2_CAP_ALGS, outputCapsAlgorithms},
+		{*fCapsHandles, pgtpm.TPM2_CAP_HANDLES, outputCapsHandles},
 	} {
 		if c.process || *fCapsAll {
-			fmt.Printf("%s:\n", capToString[c.id])
+			fmt.Printf("%s:\n", c.id.String())
 			c.cmdFunc(t)
 			fmt.Println()
 		}
@@ -33,48 +39,67 @@ func outputCaps() {
 
 // outputCapsAlgorithms outputs the algorithms supported by the TPM.
 func outputCapsAlgorithms(t io.ReadWriteCloser) {
-	vals, _, err := tpm2.GetCapability(t, tpm2.CapabilityAlgs, 1000, uint32(0))
-	if err != nil {
-		log.Fatalf("failed to get TPM algorithms: %v", err)
-	}
+	var vals []interface{}
+	var more = true
+	var err error
+	var next uint32 = 0
 
-	for _, val := range vals {
-		ad := val.(tpm2.AlgorithmDescription)
-
-		desc, ok := algToString[ad.ID]
-		if !ok {
-			desc = "<unknown>"
+	for more {
+		vals, more, err = tpm2.GetCapability(t, tpm2.CapabilityAlgs, capRequestSize, next)
+		if err != nil {
+			log.Fatalf("failed to get algorithms: %v", err)
 		}
 
-		var props []string
-		for p, d := range algPropToString {
-			if ad.Attributes&p != 0 {
-				props = append(props, strings.TrimPrefix(d, "TPMA_ALGORITHM_"))
+		for _, val := range vals {
+			ad := val.(tpm2.AlgorithmDescription)
+			next = uint32(pgtpm.Algorithm(ad.ID) + 1)
+
+			var props []string
+			for _, p := range []pgtpm.AlgorithmAttribute{
+				pgtpm.TPMA_ALGORITHM_ASYMMETRIC,
+				pgtpm.TPMA_ALGORITHM_SYMMETRIC,
+				pgtpm.TPMA_ALGORITHM_HASH,
+				pgtpm.TPMA_ALGORITHM_OBJECT,
+				pgtpm.TPMA_ALGORITHM_SIGNING,
+				pgtpm.TPMA_ALGORITHM_ENCRYPTING,
+				pgtpm.TPMA_ALGORITHM_METHOD,
+			} {
+				if pgtpm.AlgorithmAttribute(ad.Attributes)&p != 0 {
+					props = append(props, strings.TrimPrefix(p.String(), "TPMA_ALGORITHM_"))
+				}
 			}
-		}
 
-		fmt.Printf("  %-*s %s\n", 24, desc, strings.Join(props, " | "))
+			fmt.Printf("  %-*s %s\n", 24, pgtpm.Algorithm(ad.ID).String(), strings.Join(props, " | "))
+		}
 	}
 }
 
 // outputCapsHandles outputs the handles currently active in the TPM.
 func outputCapsHandles(t io.ReadWriteCloser) {
-	for _, next := range []uint32{
-		TPM2_HT_PCR,
-		TPM2_HT_NV_INDEX,
-		TPM2_HT_HMAC_SESSION,
-		TPM2_HT_POLICY_SESSION,
-		TPM2_HT_PERMANENT,
-		TPM2_HT_TRANSIENT,
-		TPM2_HT_PERSISTENT,
+	for _, ht := range []pgtpm.HandleType{
+		pgtpm.TPM2_HT_PCR,
+		pgtpm.TPM2_HT_NV_INDEX,
+		pgtpm.TPM2_HT_HMAC_SESSION,
+		pgtpm.TPM2_HT_POLICY_SESSION,
+		pgtpm.TPM2_HT_PERMANENT,
+		pgtpm.TPM2_HT_TRANSIENT,
+		pgtpm.TPM2_HT_PERSISTENT,
 	} {
-		vals, _, err := tpm2.GetCapability(t, tpm2.CapabilityHandles, 1000, next<<24)
-		if err != nil {
-			log.Fatalf("failed to get TPM handles: %v", err)
-		}
+		var vals []interface{}
+		var more = true
+		var err error
+		var next = uint32(ht.First())
 
-		for _, val := range vals {
-			fmt.Printf("  0x%08x (%s)\n", val.(tpmutil.Handle), handleTypeToString[next])
+		for more {
+			vals, more, err = tpm2.GetCapability(t, tpm2.CapabilityHandles, capRequestSize, next)
+			if err != nil {
+				log.Fatalf("failed to get handles: %v", err)
+			}
+
+			for _, val := range vals {
+				fmt.Printf("  0x%08X  %s\n", val.(tpmutil.Handle), ht.String())
+				next = uint32(val.(tpmutil.Handle)) + 1
+			}
 		}
 	}
 }
